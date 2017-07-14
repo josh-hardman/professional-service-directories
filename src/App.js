33e0a5e9d5aee9d2f4a9534fabdf8ac44680347b
Joshua Hardman <jhardman@entrata.com>
2017-07-14 15:22:04 -0600

    adding js support for themes to be passed to components and refactoring class based components to be stateless functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 //child components
 import Header from './components/Header'
@@ -6,35 +6,49 @@ import Landing from './components/Landing'
 import Listing from './components/Listing'
 import Search from './components/Search'
 
+import * as theme from './themes.js'
+
 import 'normalize.css'
 import './app.less'
 
-export default class App extends Component {
-
-  render() {
-
-    return (
-      <Router>
-          <div>
-            <Header />
-            <Switch>
-              <Route
-                exact={true}
-                path="/"
-                component={Landing}
-              />
-              <Route
-                path="/search"
-                component={Search}
-              />
-              <Route
-                path="/listing/:practiceId"
-                component={Listing}
-              />
-            </Switch>
-            <h1>Footer</h1>
-          </div>
-      </Router>
-    )
+const headerLinks = [
+  {
+    name: 'About',
+    path: '/about'
+  },
+  {
+    name: 'Contact',
+    path: '/contact'
   }
-}
+]
+
+const App = () => (
+  <Router>
+      <div>
+        <Header
+          bgColor={theme.DENTTO_BASE_BLUE}
+          header='Dentto'
+          subHeader='Discover your perfect dentist.'
+          links={headerLinks}
+        />
+        <Switch>
+          <Route
+            exact={true}
+            path="/"
+            component={Landing}
+          />
+          <Route
+            path="/search"
+            component={Search}
+          />
+          <Route
+            path="/listing/:practiceId"
+            component={Listing}
+          />
+        </Switch>
+        <h1>Footer</h1>
+      </div>
+  </Router>
+)
+
+export default App
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,45 @@
-import React, {Component} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 import './header.less'
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        <div className="header__baseline">
-          <h1 className="header__baseline__logo">Dentto</h1>
-          <p className="header__baseline__description">Discover your perfect dentist.</p>
-        </div>
-        {/* <ul>
-          <li>
-            <Link to='/'>
-              Landing
-            </Link>
-          </li>
-          <li>
-            <Link to='/search'>
-              Search
-            </Link>
-          </li>
-        </ul> */}
-      </div>
-    )
-  }
+const HeaderLink = ({link}) => (
+  <li>
+    <Link to={link.path}>
+      {link.name}
+    </Link>
+  </li>
+)
+
+HeaderLink.propTypes = {
+  link: PropTypes.object
 }
+
+const Header = ({ header, bgColor, subHeader, links }) => (
+  <div className="header" style={{background: bgColor}}>
+    <div className="header__baseline">
+      <h1 className="header__baseline__logo">{header}</h1>
+      <p className="header__baseline__description">{subHeader}</p>
+    </div>
+    <ul>
+      { links && links.map( (link, i) =>
+        <HeaderLink
+          key={i}
+          link={link}
+        />
+      ) }
+    </ul>
+  </div>
+)
+
+Header.propTypes = {
+  bgColor:    PropTypes.string,
+  header:     PropTypes.string,
+  subHeader:  PropTypes.string,
+  links:      PropTypes.arrayOf(
+                PropTypes.objects
+              )
+}
+
+export default Header
