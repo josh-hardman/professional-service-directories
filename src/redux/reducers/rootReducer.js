0c4a1105f2ceb60d376ef3e7bdd1746d4a236fa4
Joshua Hardman <jhardman0830@gmail.com>
2017-07-21 22:08:38 -0600

    removing unused redux actions and reducers due to migration to Apollo HOCs

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux'
 import filters from 'src/redux/reducers/filter'
-import filterData from 'src/redux/reducers/filterData'
-// import practices from 'src/redux/reducers/practices'
 
 export default combineReducers({
-  filterData,
   filters
-  // practices
 })
