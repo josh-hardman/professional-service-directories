2be13fcd4146cc5fd47db3efe6c7b6fefa47afd9
Joshua Hardman <jhardman0830@gmail.com>
2017-07-22 15:16:12 -0600

    Started using style components again
    Decided to look into using this again because it makes my components
    much more semantic

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import './result-card.less'
 
+const Card = styled.div`
+width: 48%;
+display: flex;
+justify-content: center;
+align-items: center;
+background: white;
+border-radius: 3px;
+box-shadow: 2px 2px 4px black;
+`
+
 const ResultCard = ({name}) => (
-  <div className="result-card">
+  <Card>
     <h4>{name}</h4>
-  </div>
+  </Card>
 )
 
 ResultCard.propTypes = {
