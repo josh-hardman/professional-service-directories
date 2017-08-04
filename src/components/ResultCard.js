81f2d5f4f4ecd65054e66ef8ca3dc78103a2803c
Joshua Hardman <jhardman0830@gmail.com>
2017-08-03 23:46:25 -0600

    Worked on ResultCard

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import {
   shadow,
-  // displayFlex,
-  // fontSize
+  displayFlex,
+  fontSize
 } from 'src/constants'
 
 const Card = styled.li`
@@ -18,68 +18,78 @@ const Card = styled.li`
   background: white;
   ${ shadow }
 `
-//
-// const InfoSection = styled.div`
-//   width: 100%;
-//   height: 160px;
-//   padding: 12px;
-//   border: 1px solid teal;
-//   ${ displayFlex() };
-//   justify-content: space-around;
-// `
-//
-// const Image = styled.div`
-//   flex: 1;
-//   height: 140px;
-//   border: 1px solid grey;
-// `
-//
-// const Info = styled.div`
-//   flex: 2;
-//   width: 140px;
-//   height: 140px;
-//   border: 1px solid green;
-//   padding: 120px;
-// `
-//
-// const PracticeName = styled.h1`
-//   width: 100%;
-//   margin: 0;
-//   ${ fontSize(18) }
-// `
-//
-// const Rating = styled.p`
-//   width: 100%;
-//   margin: 0;
-//   ${ fontSize(12) }
-// `
-//
-// const Social = styled.ul`
-//   list-style: none;
-//   padding: 0;
-//   ${ fontSize(12) }
-// `
-//
-// const MediaLink = styled.li`
-//
-// `
 
-const ResultCard = ({ name, image }) => (
+const InfoSection = styled.div`
+  width: 100%;
+  height: 160px;
+  padding: 12px;
+  ${'' /* border: 1px solid red; */}
+`
+
+const Frame = styled.div`
+  position: relative;
+  overflow: hidden;
+  width: 25%;
+  height: 140px;
+  float: left;
+  ${'' /* max-width: 200px; */}
+`
+
+const Image = styled.img`
+  display: block;
+  margin: 0 auto;
+  height: 100%;
+  width: auto;
+  border-radius: 3px;
+`
+
+const Info = styled.div`
+  width: 75%;
+  height: 100%;
+  ${'' /* border: 1px solid green; */}
+  padding: 12px;
+  float: left;
+`
+
+const PracticeName = styled.h1`
+  width: 100%;
+  margin: 0;
+  font-weight: 400;
+  ${ fontSize(18) }
+`
+
+const Rating = styled.p`
+  width: 100%;
+  margin: 0;
+  ${ fontSize(12) }
+`
+
+const Social = styled.ul`
+  list-style: none;
+  padding: 0;
+  ${ fontSize(12) }
+`
+
+const MediaLink = styled.li`
+
+`
+
+const ResultCard = ({ name }) => (
   <Card>
-    <span>{name}</span>
-    <img src={image}></img>
-    {/* <InfoSection>
-      <Image />
+    <InfoSection>
+      <Frame>
+        <Image src='https://expertbeacon.com/sites/default/files/pain-free_dentistry_can_help_you_get_over_your_fear_of_the_dentist.jpg'/>
+      </Frame>
       <Info>
         <PracticeName>{name}</PracticeName>
-        <Rating>ratingz: {rating ? rating : 'N/A'}</Rating>
-        { social &&
+        {/* <Rating>ratingz: {rating ? rating : 'N/A'}</Rating> */}
+        {/* { social &&
           <Social>
             <MediaLink>{social.website}</MediaLink>
           </Social>
-        }
+        } */}
       </Info>
-    </InfoSection> */}
+    </InfoSection>
   </Card>
 )
 
