69d0c4f5f2b5e4b9e23ee7599dd175c6651ef68d
Joshua Hardman <jhardman0830@gmail.com>
2017-08-21 22:37:47 -0600

    Adding Card component

---
+++ src/components/Card.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { colors, displayFlex, shadow } from 'src/constants'
+
+const CardWrapper = styled.li`
+  width: ${({width}) => {
+    if( width == 'half' ) return '50%'
+    else if( width == 'third' ) return '33.3333333333333%'
+    else if( width == 'full' ) return '100%'
+  }};
+  ${displayFlex()}
+  justify-content: center;
+  padding: 2px;
+`
+
+const StyledCard = styled.div`
+  background-color: ${colors.white};
+  ${shadow()}
+  width: 100%;
+  max-width: 950px;
+  padding: 18px;
+`
+
+const Card = ({width, children}) => (
+  <CardWrapper width={width}>
+    <StyledCard>
+      {children}
+    </StyledCard>
+  </CardWrapper>
+)
+
+Card.propTypes = {
+  width:    PropTypes.string,
+  children: PropTypes.node
+}
+
+export default Card
