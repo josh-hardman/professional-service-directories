dd1e09fd75b4179adf140939b542aa1539ac9115
Joshua Hardman <jhardman0830@gmail.com>
2017-08-21 22:37:37 -0600

    Adding Backdrop component

---
+++ src/components/Backdrop.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { colors, displayFlex } from 'src/constants'
+
+const BackdropWrapper = styled.div`
+  width: 100%;
+  ${displayFlex()}
+  justify-content: center;
+  padding-top: 18px;
+  padding-bottom: 18px;
+`
+
+const StyledBackdrop = styled.div`
+  background-color: ${colors.gray};
+  width: 100%;
+  max-width: 950px;
+  padding: 18px;
+`
+
+const Backdrop = ({children}) => (
+  <BackdropWrapper>
+    <StyledBackdrop>
+      {children}
+    </StyledBackdrop>
+  </BackdropWrapper>
+)
+
+Backdrop.propTypes = {
+  children: PropTypes.node
+}
+
+export default Backdrop
